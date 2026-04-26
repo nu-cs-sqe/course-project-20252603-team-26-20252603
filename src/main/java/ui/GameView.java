5a2c4c3f4f6b273d83d9fad59a18cba281bd8e3d
sidneyrobinson52 <sidneyrobinson2026@u.northwestern.edu>
2026-04-26 13:31:31 -0500

    Setup GameView

---
+++ src/main/java/ui/GameView.java
@@ -0,0 +1,8 @@
+package ui;
+
+public class GameView {
+    public GameView() {
+
+    }
+
+}
--- src/main/java/ui/Main.java
+++ src/main/java/ui/Main.java
@@ -1,1 +1,8 @@
+package ui;
 
+public class Main {
+    // testing the GameView UI via main
+    public static void main(String[] args){
+        GameView view = new GameView();
+    }
+}
