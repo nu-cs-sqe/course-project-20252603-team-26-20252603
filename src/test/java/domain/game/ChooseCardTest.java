831995ab0596fd8a2c3ecda226762c63735d8fad
Seungyeon Shin <syshin4072@gmail.com>
2026-05-04 05:24:57 -0500

    chooseCard_FirstIndex_ReturnsDefuseExplanation() done

--- src/test/java/domain/game/ChooseCardTest.java
+++ src/test/java/domain/game/ChooseCardTest.java
@@ -12,6 +12,18 @@ public class ChooseCardTest {
         hand = new Hand();
         hand.addCard(new Card(CardType.EXPLODING_KITTEN));
         hand.addCard(new Card(CardType.DEFUSE));
-        hand.addCard(new Card(CardType.OTHER));
+        hand.addCard(new Card(CardType.PLACEHOLDER_CARD));
     }
+
+    @Test
+    void chooseCard_FirstIndex_ReturnsDefuseExplanation() {
+        String explanation = hand.chooseCard(0);
+
+        assertEquals(
+                "Defuse: Use this card to avoid exploding after drawing an Exploding Kitten.",
+                explanation
+        );
+        assertEquals(3, hand.getHandSize());
+    }
+
 }
