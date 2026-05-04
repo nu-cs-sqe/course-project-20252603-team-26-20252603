beb5fd19e63121a79f54e3c9e29a95917dc9d6a8
Seungyeon Shin <syshin4072@gmail.com>
2026-05-04 06:18:18 -0500

    done with canSubmitCard_OneBeardCat_ReturnsFalse()

--- src/test/java/domain/game/CatCardTest.java
+++ src/test/java/domain/game/CatCardTest.java
@@ -1,4 +1,16 @@
 package domain.game;
 
-public class CatCardTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+class CatCardTest {
+    @Test
+    void canSubmitCard_OneBeardCat_ReturnsFalse() {
+        Player player = new Player("Sophie");
+        player.addCard(new Card(CardType.BEARD_CAT));
+
+        assertEquals(false, player.canSubmitCard(0));
+    }
 }
