c82ef15f1dbc0f41495d6b0f0a86131f499efcec
aliviawynn2024 <aliviawynn@gmail.com>
2026-05-06 12:20:15 -0500

    opening draft pr

--- src/main/java/domain/game/Card.java
+++ src/main/java/domain/game/Card.java
@@ -4,6 +4,7 @@ import java.util.Objects;
 
 public class Card {
     private final CardType type;
+    //hi
 
     public Card(CardType type) {
     this.type = Objects.requireNonNull(type, "type must not be null");
