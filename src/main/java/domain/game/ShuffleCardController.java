169c24c5864ad9c345e2e2131ce9258f644164b3
bc2k13 <bctwok13@gmail.com>
2026-05-10 12:00:02 -0500

    play_NonShuffleCard_ThrowsExceptionAndLeavesStateUnchanged passes

--- src/main/java/domain/game/ShuffleCardController.java
+++ src/main/java/domain/game/ShuffleCardController.java
@@ -1,9 +1,14 @@
 package domain.game;
 
 public final class ShuffleCardController {
+    private static final String NOT_SHUFFLE_CARD_MESSAGE = "selected card is not a shuffle card";
+
     public void play(Game game, int cardIndex) {
         Player currentPlayer = game.getCurrentPlayer();
         Card shuffleCard = currentPlayer.getHandSnapshot().get(cardIndex);
+        if (shuffleCard.getType() != CardType.SHUFFLE) {
+            throw new IllegalArgumentException(NOT_SHUFFLE_CARD_MESSAGE);
+        }
 
         currentPlayer.removeCard(cardIndex);
         game.getDiscardPile().add(shuffleCard);
