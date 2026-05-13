d49e7ba8ffab89b02860372af35b677010de45bf
Seungyeon Shin <syshin4072@gmail.com>
2026-05-13 01:15:55 -0500

    peekTopCards_EmptyDeck_ReturnsEmptyList passes

--- src/test/java/domain/game/SeeFutureControllerTest.java
+++ src/test/java/domain/game/SeeFutureControllerTest.java
@@ -1,4 +1,17 @@
 package domain.game;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SeeFutureControllerTest {
+    @Test
+    void peekTopCards_EmptyDeck_ReturnsEmptyList() {
+        Deck deck = new Deck(List.of());
+        List<Card> viewedCards = deck.peekTopCards(2);
+
+        assertEquals(0, viewedCards.size());
+    }
 }
