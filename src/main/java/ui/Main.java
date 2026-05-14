d56c67715b2078703ed4171e0f68def6a3378177
bc2k13 <bctwok13@gmail.com>
2026-05-14 04:16:05 -0500

    Fix Main Checkstyle spacing

--- src/main/java/ui/Main.java
+++ src/main/java/ui/Main.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class Main {
     // testing the GameView UI via main
-    public static void main(String[] args){
+    public static void main(String[] args) {
         GameView view = new GameView();
 
         // TC 1 "displayStartScreen_DisplayOnce_ShowsTitle" from BVA GameView
