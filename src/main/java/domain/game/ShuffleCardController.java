3bd644508ccb00ef9f4836a1a21ba4cefff5e863
Seungyeon Shin <syshin4072@gmail.com>
2026-05-28 17:51:13 -0500

    style: remove discussion comments from shuffle controller

--- src/main/java/domain/game/ShuffleCardController.java
+++ src/main/java/domain/game/ShuffleCardController.java
@@ -16,8 +16,7 @@ public final class ShuffleCardController {
 
         currentPlayer.removeCard(cardIndex);
         game.getDiscardPile().add(shuffleCard);
-        // The physical "tell you to stop" step is modeled as one complete random draw-pile shuffle.
-        // if we wanted it to be exactly like the rules, it would be a way more complicated implementation involving asking the next player to "stop"
+
         game.getDrawPile().shuffle();
     }
 }
