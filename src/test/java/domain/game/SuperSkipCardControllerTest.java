b49bef87498e90eabc8324d3a36d41f613ae0924
sidneyrobinson52 <sidneyrobinson2026@u.northwestern.edu>
2026-06-07 16:03:49 -0500

    playSuperSkip_NonSuperSkipCard_ThrowsException passes

--- src/test/java/domain/game/SuperSkipCardControllerTest.java
+++ src/test/java/domain/game/SuperSkipCardControllerTest.java
@@ -1,4 +1,19 @@
 package domain.game;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class SuperSkipCardControllerTest {
+    @Test
+    void playSuperSkip_NonSuperSkipCard_ThrowsException() {
+        Player player = new Player("Gojo");
+        player.addCard(new Card(CardType.ATTACK));
+
+        DiscardPile discardPile = new DiscardPile();
+        SuperSkipCardController controller = new SuperSkipCardController(discardPile);
+
+        assertThrows(IllegalArgumentException.class, () -> controller.play(player, 0));
+    }
+
 }
