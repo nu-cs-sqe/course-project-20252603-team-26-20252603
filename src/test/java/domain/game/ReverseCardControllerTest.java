96c1015579e36160c732e772015c21e91df6e643
sidneyrobinson52 <sidneyrobinson2026@u.northwestern.edu>
2026-06-07 23:45:55 -0500

    fixed checkstyle error

--- src/test/java/domain/game/ReverseCardControllerTest.java
+++ src/test/java/domain/game/ReverseCardControllerTest.java
@@ -2,7 +2,10 @@ package domain.game;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class ReverseCardControllerTest {
     @Test
