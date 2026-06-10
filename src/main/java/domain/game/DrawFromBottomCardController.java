250ad002bbae3732676d60208af726c25195a7ce
aliviawynn2024 <aliviawynn@gmail.com>
2026-06-10 00:28:59 -0500

    play_NegativeIndex_ThrowsIllegalArgumentException passes

---
+++ src/main/java/domain/game/DrawFromBottomCardController.java
@@ -0,0 +1,26 @@
+package domain.game;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
+public class DrawFromBottomCardController {
+    private static final String NOT_DRAW_FROM_BOTTOM_CARD_MESSAGE = "selected card is not a Draw from bottom card";
+    private static final String INVALID_INDEX_MESSAGE = "cardIndex is out of bounds";
+    private final Deck deck;
+    private final DiscardPile discardPile;
+
+
+    @SuppressFBWarnings("EI")
+    public DrawFromBottomCardController(Deck deck, DiscardPile discardPile) {
+        this.discardPile = discardPile;
+        this.deck = deck;
+    }
+
+    public Card play(Player player, int cardIndex){
+        if (cardIndex < 0 || cardIndex >= player.getHandSize()) {
+            throw new IllegalArgumentException(INVALID_INDEX_MESSAGE);
+        }
+        return new Card(CardType.PLACEHOLDER_CARD);
+
+    }
+
+}
