f6a903de998a0bd2fc93b819438ec487661819ed
aliviawynn2024 <aliviawynn@gmail.com>
2026-06-10 02:05:56 -0500

    play_IndexEqualsHandSize_ThrowsIllegalArgumentException passes

--- src/test/java/domain/game/TargetedAttackCardControllerTest.java
+++ src/test/java/domain/game/TargetedAttackCardControllerTest.java
@@ -16,6 +16,15 @@ public class TargetedAttackCardControllerTest {
         assertThrows(IllegalArgumentException.class, () -> controller.play(player, -1));
     }
 
+    @Test
+    void play_IndexEqualsHandSize_ThrowsIllegalArgumentException() {
+        Player player = new Player("Sophie");
+        player.addCard(new Card(CardType.TARGETED_ATTACK));
+        DiscardPile discardPile = new DiscardPile();
+        TargetedAttackCardController controller = new TargetedAttackCardController(discardPile);
+
+        assertThrows(IllegalArgumentException.class, () -> controller.play(player, player.getHandSize()));
+    }
 
 }
 
